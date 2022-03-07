8c24c75d677d46f237e2b3e4ee5ab4056019d2f2
Ivin-Jogi <ivin.jogi@ibsplc.com>
2022-03-07 20:55:08 +0530

    try change

--- src/main/java/com/ibs/litmusproject/hotelbooking/service/SearchListService.java
+++ src/main/java/com/ibs/litmusproject/hotelbooking/service/SearchListService.java
@@ -24,7 +24,6 @@ public class SearchListService {
 
        // String rapidUrl = "https://hotels4.p.rapidapi.com/properties/list?destinationId=1506246&pageNumber=1&pageSize=25&checkIn=2020-01-08&checkOut=2020-01-15&adults1=1&sortOrder=PRICE&locale=en_US&currency=USD";
         String apiUrl = "https://hotels4.p.rapidapi.com/properties/list";
-        //apiUrl = apiUrl + "?" + getApiUrl("1506246", "2020-01-08", "2020-01-15");
         apiUrl = apiUrl + "?" + "destinationId="+destId+"&pageNumber=1&pageSize=10&checkIn="+checkIn+"&checkOut="+checkOut ;
         RestTemplate restTemplate = new RestTemplate();
 
